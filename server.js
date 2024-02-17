54f232786ecb2862f6164d1933d509ddc4597d08
amrit-raaj <76815978+amrit-raaj@users.noreply.github.com>
2024-02-17 15:07:47 +0530

    version1.0

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import color from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+import path from "path";
+const app = express();
+
+dotenv.config();
+
+connectDB();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome dear friend</h1>");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
